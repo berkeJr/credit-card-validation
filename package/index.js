f37cf0247818f9bf8cb05e9dc2c952c81c6718f5
Berke Sayın <sayinberkesayin@gmail.com>
2023-07-23 13:53:24 +0300

    Add function to validate credit card number

---
+++ package/index.js
@@ -0,0 +1,28 @@
+const validateCreditCardNumber = (input) => {
+  // Convert card number to numerical range (array)
+  let creditCardInt = input.split("").map(Number);
+
+  // Implement Luhn Algorithm
+  // Product each digit with 2 starting from the end
+  for (let i = creditCardInt.length - 2; i >= 0; i = i - 2) {
+    let tempValue = creditCardInt[i];
+    tempValue = tempValue * 2; // Product chosen digit with 2
+
+    if (tempValue > 9) {
+      tempValue = (tempValue % 10) + 1;
+    }
+
+    creditCardInt[i] = tempValue;
+  }
+
+  // Calculate the total of each digits
+  let total = 0;
+  for (let i = 0; i < creditCardInt.length; i++) {
+    total += creditCardInt[i];
+  }
+
+  // Check if the card number is valid
+  return total % 10 === 0;
+};
+
+module.exports = { validateCreditCardNumber }
